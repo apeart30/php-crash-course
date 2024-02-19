44ff52804a80520306f4cbec73c8e4cad95faef2
apeart30 <murengerantwarijames@gmail.com>
2024-02-19 19:33:19 +0200

    all types of commenting in php
    i added all types of commenting in php in order to keep track of diffent styles you can use php comments

--- basics/helloworld.php
+++ basics/helloworld.php
@@ -1,4 +1,8 @@
 <?php
 // all types of commect in php
+//the first one has already been seen
+#the second one is this .. which i kind of like due to its familiarity with bash or python like the shebang thing
+/*the third one is multiline comment and this is how it works
+its very weird i don't even like how its written like who would do such things*/
 echo "Hello World\n";
 ?>
