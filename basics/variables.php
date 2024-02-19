ea2ed812f3ca12d74076b108aa797253a74bbf50
apeart30 <murengerantwarijames@gmail.com>
2024-02-19 19:53:01 +0200

    variable in php
    four main variable data types in php , more complex variables will come as i progress on the learning journey of php
    and again this is for code review purposes.. i don't want to be no damn coder but we never know we'll see where that leads

--- basics/variables.php
+++ basics/variables.php
@@ -4,5 +4,13 @@
 $name="john";
 
 echo "hello $name";
+#now we're gonna learn more about the varibles with types
+#php supports various datatypes
+$string="hello";
+$number=10;
+$float=10.23;
+$boolean=True;
+echo "$string $number $float $boolean \n";
+#the above is the main four i'll add more as i move on to complex topics
 
 ?>
