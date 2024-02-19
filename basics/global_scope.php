6ad6168925794dfae30e605005a53e562979e338
apeart30 <murengerantwarijames@gmail.com>
2024-02-19 20:32:13 +0200

    changed the global_scope file into accepting global variable
    i used the global keyword to demonstrate how you can use the the global variable inside
    a function in php
    now its fucking working

--- basics/global_scope.php
+++ basics/global_scope.php
@@ -3,7 +3,11 @@ $x = 5; // global scope
 
 function myTest() {
   // using x inside this function will generate an error
-  echo "<p>Variable x inside function is: $x</p>";
+// here's to demonstrating how global variables can be accessed inside a function using global keyword
+global $x;
+// now the program won't produce any errors since the variable is included in the function
+
+echo "<p>Variable x inside function is: $x</p>";
 }
 myTest();
 
