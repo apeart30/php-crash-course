248093b7ecaf5f8ddc06b1955302735ba65cb0b3
apeart30 <murengerantwarijames@gmail.com>
2024-03-08 20:07:41 +0200

    added a for loop file trying to understand parts of a large problem

---
+++ basics/new/for_loop.php
@@ -0,0 +1,9 @@
+<?php
+#for loops are only suitable to execute when you know exactly how
+#many times a loop is supposed to occur
+#lets see that
+
+for($x=0;$x<=20;$x++){
+echo "this statement will break$x\n";
+}
+?>
---
+++ basics/new/while_continue.php
@@ -0,0 +1,8 @@
+<?php
+$i=0;
+while($i<20){
+$i++;
+if ($i==3) continue;
+echo "$i\n";
+}
+?>
